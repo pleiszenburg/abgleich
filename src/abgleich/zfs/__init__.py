1e2cbeacc6689a208aceec6c8bb2f1c1bc9ecbce
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-07-17 11:29:30 +0200

    added receive routines

--- src/abgleich/zfs/__init__.py
+++ src/abgleich/zfs/__init__.py
@@ -42,3 +42,40 @@ def parse_table(raw):
 	head = ['NAME', 'USED', 'AVAIL', 'REFER', 'MOUNTPOINT']
 	table = [item.split('\t') for item in raw.split('\n') if len(item.strip()) > 0]
 	return [{k: v for k, v in zip(head, line)} for line in table]
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ROUTINES: SEND & RECEIVE
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def receive_snapshot(host, src, src_firstsnapshot, dest):
+	print('RECEIVING FIRST %s@%s to %s ...' % (src, src_firstsnapshot, dest))
+	cmd1 = ssh_command(
+		host,
+		['zfs', 'send', '-c', '%s@%s' % (src, src_firstsnapshot)],
+		compression = False
+		)
+	cmd2 = ['zfs', 'receive', '%s' % dest]
+	run_chain_command(cmd1, cmd2)
+	print('... DONE.')
+
+def receive_snapshot_incremental(host, src, src_a, src_b, dest):
+	print('RECEIVING FOLLOW-UP %s@[%s - %s] to %s ...' % (src, src_a, src_b, dest))
+	cmd1 = ssh_command(
+		host,
+		['zfs', 'send', '-c', '-i', '%s@%s' % (src, src_a), '%s@%s' % (src, src_b)],
+		compression = False
+		)
+	cmd2 = ['zfs', 'receive', '%s' % dest]
+	run_chain_command(cmd1, cmd2)
+	print('... DONE.')
+
+def receive_new(host, dataset_src, dest):
+	src = dataset_src['NAME']
+	src_firstsnapshot = dataset_src['SNAPSHOTS'][0]['NAME']
+	src_snapshotpairs = [
+		(a['NAME'], b['NAME'])
+		for a, b in zip(dataset_src['SNAPSHOTS'][:-1], dataset_src['SNAPSHOTS'][1:])
+		]
+	receive_snapshot(host, src, src_firstsnapshot, dest)
+	for src_a, src_b in src_snapshotpairs:
+		receive_snapshot_incremental(host, src, src_a, src_b, dest)
