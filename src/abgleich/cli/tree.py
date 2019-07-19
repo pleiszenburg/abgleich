cce572cf2fb4de6eafc18a55e81731caa8d3f628
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-07-19 08:48:38 +0200

    improved tree rendering with meta data

--- src/abgleich/cli/tree.py
+++ src/abgleich/cli/tree.py
@@ -7,6 +7,7 @@
 import click
 from tabulate import tabulate
 
+from ..io import humanize_size
 from ..zfs import get_tree
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -16,14 +17,24 @@ from ..zfs import get_tree
 @click.command(short_help = 'show dataset tree')
 @click.argument('host', default = 'localhost', type = str)
 def tree(host):
+	cols = ['NAME', 'USED', 'REFER', 'compressratio']
+	col_align = ('left', 'right', 'right', 'decimal')
+	size_cols = ['USED', 'REFER']
 	datasets = get_tree(host if host != 'localhost' else None)
 	table = []
 	for dataset in datasets:
-		table.append([dataset['NAME']])
+		table.append([
+			dataset[col] if col not in size_cols else humanize_size(int(dataset[col]))
+			for col in cols
+			])
 		for snapshot in dataset['SNAPSHOTS']:
-			table.append(['- ' + snapshot['NAME']])
+			table.append(['- ' + snapshot['NAME']] + [
+				snapshot[col] if col not in size_cols else humanize_size(int(snapshot[col]))
+				for col in cols[1:]
+				])
 	print(tabulate(
 		table,
-		headers = ['NAME'],
-		tablefmt = 'github'
+		headers = cols,
+		tablefmt = 'github',
+		colalign = col_align
 		))
