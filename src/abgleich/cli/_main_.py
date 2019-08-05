039a890cef9b45c1279c2f41d73e73f3b127aecd
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-05 14:05:04 +0200

    added headers 3

--- src/abgleich/cli/_main_.py
+++ src/abgleich/cli/_main_.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-8 -*-
 
+"""
+
+ABGLEICH
+zfs sync tool
+https://github.com/pleiszenburg/abgleich
+
+	src/abgleich/cli/_main_.py: CLI auto-detection
+
+	Copyright (C) 2019 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/abgleich/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # IMPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -26,6 +51,6 @@ def _add_commands(ctx):
 
 @click.group()
 def cli():
-	"""abgleich, btrfs and zfs sync tool"""
+	"""abgleich, zfs sync tool"""
 
 _add_commands(cli)
