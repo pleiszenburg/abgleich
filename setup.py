07df04f9f768e472d99972acc0cbe8f5d7b68f46
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-05 14:59:36 +0200

    new release cycle

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ import os
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # Package version
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 # List all versions of Python which are supported
 confirmed_python_versions = [
