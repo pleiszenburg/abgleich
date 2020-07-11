a6eaa5382abc51c6a67391e7d1b40815d910b8f4
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-07-11 16:16:44 +0200

    tree cli runs on new logic

--- src/abgleich/cli/tree.py
+++ src/abgleich/cli/tree.py
@@ -30,35 +30,45 @@ specific language governing rights and limitations under the License.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import click
-from tabulate import tabulate
+# from tabulate import tabulate
 
-from ..io import colorize, humanize_size
-from ..zfs import get_tree
+# from ..io import colorize, humanize_size
+# from ..zfs import get_tree
+
+from ..config import Config
+from ..zfs.zpool import Zpool
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ROUTINES
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# @click.command(short_help="show dataset tree")
+# @click.argument("host", default="localhost", type=str)
+# def tree(host):
 
 @click.command(short_help="show dataset tree")
-@click.argument("host", default="localhost", type=str)
-def tree(host):
-    cols = ["NAME", "USED", "REFER", "compressratio"]
-    col_align = ("left", "right", "right", "decimal")
-    size_cols = ["USED", "REFER"]
-    datasets = get_tree(host if host != "localhost" else None)
-    table = []
-    for dataset in datasets:
-        table.append([dataset[col] for col in cols])
-        for snapshot in dataset["SNAPSHOTS"]:
-            table.append(
-                ["- " + snapshot["NAME"]] + [snapshot[col] for col in cols[1:]]
-            )
-    for row in table:
-        for col in [1, 2]:
-            row[col] = humanize_size(int(row[col]), add_color=True)
-        if not row[0].startswith("- "):
-            row[0] = colorize(row[0], "white")
-        else:
-            row[0] = colorize(row[0], "grey")
-    print(tabulate(table, headers=cols, tablefmt="github", colalign=col_align))
+@click.argument("configfile", type=click.File("r", encoding="utf-8"))
+@click.argument("side", default="source", type=str)
+def tree(configfile, side):
+
+	zpool = Zpool.from_config(side, config = Config.from_fd(configfile))
+
+    # cols = ["NAME", "USED", "REFER", "compressratio"]
+    # col_align = ("left", "right", "right", "decimal")
+    # size_cols = ["USED", "REFER"]
+    # datasets = get_tree(host if host != "localhost" else None)
+    # table = []
+    # for dataset in datasets:
+    #     table.append([dataset[col] for col in cols])
+    #     for snapshot in dataset["SNAPSHOTS"]:
+    #         table.append(
+    #             ["- " + snapshot["NAME"]] + [snapshot[col] for col in cols[1:]]
+    #         )
+    # for row in table:
+    #     for col in [1, 2]:
+    #         row[col] = humanize_size(int(row[col]), add_color=True)
+    #     if not row[0].startswith("- "):
+    #         row[0] = colorize(row[0], "white")
+    #     else:
+    #         row[0] = colorize(row[0], "grey")
+    # print(tabulate(table, headers=cols, tablefmt="github", colalign=col_align))
