60062538a3b439374f1ae4ad470b1ccfef0f460e
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-07-12 21:49:33 +0200

    prepare new snap cli

--- src/abgleich/cli/snap.py
+++ src/abgleich/cli/snap.py
@@ -30,16 +30,9 @@ specific language governing rights and limitations under the License.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import click
-from tabulate import tabulate
-import yaml
-from yaml import CLoader
 
-from ..io import colorize, humanize_size
-from ..zfs import (
-    create_snapshot,
-    get_tree,
-    get_snapshot_tasks,
-)
+from ..config import Config
+from ..zfs.zpool import Zpool
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ROUTINES
@@ -50,26 +43,28 @@ from ..zfs import (
 @click.argument("configfile", type=click.File("r", encoding="utf-8"))
 def snap(configfile):
 
-    config = yaml.load(configfile.read(), Loader=CLoader)
+    zpool = Zpool.from_config('source', config = Config.from_fd(configfile))
 
-    cols = ["NAME", "written", "FUTURE SNAPSHOT"]
-    col_align = ("left", "right")
-    datasets = get_tree()
-    snapshot_tasks = get_snapshot_tasks(
-        datasets, config["prefix_local"], config["ignore"]
-    )
-
-    table = []
-    for name, written, snapshot_name in snapshot_tasks:
-        table.append([name, humanize_size(written, add_color=True), snapshot_name])
-
-    print(tabulate(table, headers=cols, tablefmt="github", colalign=col_align))
-
-    click.confirm("Do you want to continue?", abort=True)
-
-    for name, _, snapshot_name in snapshot_tasks:
-        create_snapshot(
-            config["prefix_local"] + name,
-            snapshot_name,
-            # debug = True
-        )
+    # config = yaml.load(configfile.read(), Loader=CLoader)
+	#
+    # cols = ["NAME", "written", "FUTURE SNAPSHOT"]
+    # col_align = ("left", "right")
+    # datasets = get_tree()
+    # snapshot_tasks = get_snapshot_tasks(
+    #     datasets, config["prefix_local"], config["ignore"]
+    # )
+	#
+    # table = []
+    # for name, written, snapshot_name in snapshot_tasks:
+    #     table.append([name, humanize_size(written, add_color=True), snapshot_name])
+	#
+    # print(tabulate(table, headers=cols, tablefmt="github", colalign=col_align))
+	#
+    # click.confirm("Do you want to continue?", abort=True)
+	#
+    # for name, _, snapshot_name in snapshot_tasks:
+    #     create_snapshot(
+    #         config["prefix_local"] + name,
+    #         snapshot_name,
+    #         # debug = True
+    #     )
