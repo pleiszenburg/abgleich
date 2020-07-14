fd3b5f36118a79fc76ba6438a62058ee14e309c2
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-07-14 12:54:19 +0200

    cleanup runs on new cli

--- src/abgleich/cli/cleanup.py
+++ src/abgleich/cli/cleanup.py
@@ -6,9 +6,9 @@ ABGLEICH
 zfs sync tool
 https://github.com/pleiszenburg/abgleich
 
-	src/abgleich/cli/cleanup.py: cleanup command entry point
+    src/abgleich/cli/cleanup.py: cleanup command entry point
 
-	Copyright (C) 2019-2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
+    Copyright (C) 2019-2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
 <LICENSE_BLOCK>
 The contents of this file are subject to the GNU Lesser General Public License
@@ -30,16 +30,9 @@ specific language governing rights and limitations under the License.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import click
-from tabulate import tabulate
-import yaml
-from yaml import CLoader
 
-from ..io import colorize, humanize_size
-from ..zfs import (
-    get_tree,
-    get_cleanup_tasks,
-    delete_snapshot,
-)
+from ..config import Config
+from ..zfs.zpool import Zpool
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ROUTINES
@@ -50,32 +43,18 @@ from ..zfs import (
 @click.argument("configfile", type=click.File("r", encoding="utf-8"))
 def cleanup(configfile):
 
-    config = yaml.load(configfile.read(), Loader=CLoader)
+    config = Config.from_fd(configfile)
 
-    cols = ["NAME", "DELETE SNAPSHOT"]
-    col_align = ("left", "left")
-    datasets = get_tree()
-    cleanup_tasks = get_cleanup_tasks(
-        datasets, config["prefix_local"], config["ignore"], config["keep_snapshots"]
-    )
-    space_before = int(datasets[0]["AVAIL"])
+    source_zpool = Zpool.from_config('source', config = config)
+    target_zpool = Zpool.from_config('target', config = config)
 
-    table = []
-    for name, snapshot_name in cleanup_tasks:
-        table.append([name, snapshot_name])
+    transactions = source_zpool.get_cleanup_transactions(target_zpool)
 
-    print(tabulate(table, headers=cols, tablefmt="github", colalign=col_align))
-    print("%s available" % humanize_size(space_before, add_color=True))
+    if len(transactions) == 0:
+        print('nothing to do')
+        return
+    transactions.print_table()
 
     click.confirm("Do you want to continue?", abort=True)
 
-    for name, snapshot_name in cleanup_tasks:
-        delete_snapshot(
-            config["prefix_local"] + name,
-            snapshot_name,
-            # debug = True
-        )
-
-    space_after = int(get_tree()[0]["AVAIL"])
-    print("%s available" % humanize_size(space_before, add_color=True))
-    print("%s freed" % humanize_size(space_before - space_before, add_color=True))
+    transactions.run()
