9f658c2459dfe5231a383595ebf79c66469ae704
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-07-17 19:00:27 +0200

    draft table model

--- src/abgleich/gui/transaction.py
+++ src/abgleich/gui/transaction.py
@@ -23,3 +23,71 @@ specific language governing rights and limitations under the License.
 </LICENSE_BLOCK>
 
 """
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import typing
+
+import typeguard
+from PyQt5.QtCore import QAbstractTableModel, QModelIndex, Qt
+
+from ..core.abc import TransactionListABC
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CLASS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+@typeguard.typechecked
+class TransactionListModel(QAbstractTableModel):
+
+    def __init__(self, transactions: TransactionListABC):
+
+        super().__init__()
+        self._transactions = transactions
+        self._transactions.changed = self._changed
+
+        self._rows, self._cols = None, None
+        self._update()
+
+    def data(self, index: QModelIndex, role: int) -> typing.Union[None, str]: # TODO return type
+
+        row, col = index.row(), index.column()
+        col_key = self._cols[col]
+
+        if role == Qt.DisplayRole:
+            if col_key != 'status':
+                return str(self._transactions[row].meta[col_key]) # TODO format ...
+            else:
+                return 'no idea' # TODO actual
+
+    def headerData(self, section: int, orientation: Qt.Orientation, role: int) -> typing.Union[None, str]:
+
+        if role == Qt.DisplayRole:
+
+            if orientation == Qt.Horizontal:
+                return self._cols[section]
+
+            if orientation == Qt.Vertical:
+                return self._rows[section]
+
+    def rowCount(self, index: QModelIndex) -> int:
+
+        return len(self._transactions)
+
+    def columnCount(self, index: QModelIndex) -> int:
+
+        return len(self._cols)
+
+    def _changed(self):
+
+        self._update()
+        self.layoutChanged.emit()
+        # self.model.dataChanged.emit(index, index)
+
+    def _update(self):
+
+        self._rows = self._transactions.table_rows
+        self._cols = self._transactions.table_columns
+        self._cols.append('status')
