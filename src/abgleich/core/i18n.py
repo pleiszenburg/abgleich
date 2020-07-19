0589db9a2e541a9940ecac4876f4fea7e9fe5a09
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-07-19 17:23:00 +0200

    added translation hack

---
+++ src/abgleich/core/i18n.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+ABGLEICH
+zfs sync tool
+https://github.com/pleiszenburg/abgleich
+
+    src/abgleich/core/i18n.py: Translations
+
+    Copyright (C) 2019-2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/abgleich/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import locale
+import os
+
+import typeguard
+import yaml
+from yaml import CLoader
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CONST
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+_LANG = locale.getlocale()[0].split('_')[0]
+with open(os.path.join(os.path.dirname(__file__), 'translations.yaml')) as _f:
+    _TEXT = yaml.load(_f.read(), Loader=CLoader)
+del _f
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ROUTINES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+@typeguard.typechecked
+def t(name: str) -> str:
+
+    return  _TEXT.get(name, {}).get(_LANG, name)
