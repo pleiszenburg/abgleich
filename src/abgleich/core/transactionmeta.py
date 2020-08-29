3cb95ae2d9c410d758c077dac615d760436cf49a
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-08-29 18:55:12 +0200

    broken transaction classes into modules

---
+++ src/abgleich/core/transactionmeta.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+ABGLEICH
+zfs sync tool
+https://github.com/pleiszenburg/abgleich
+
+    src/abgleich/core/transactionmeta.py: ZFS transaction meta
+
+    Copyright (C) 2019-2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/abgleich/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from typing import Generator, Union
+
+from typeguard import typechecked
+
+from .abc import TransactionMetaABC
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# TYPES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+TransactionMetaTypes = Union[str, int, float]
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CLASS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+@typechecked
+class TransactionMeta(TransactionMetaABC):
+    """
+    Immutable.
+    """
+
+    def __init__(self, **kwargs: TransactionMetaTypes):
+
+        self._meta = kwargs
+
+    def __getitem__(self, key: str) -> TransactionMetaTypes:
+
+        return self._meta[key]
+
+    def __len__(self) -> int:
+
+        return len(self._meta)
+
+    def get(self, key: str) -> Union[TransactionMetaTypes, None]:
+
+        return self._meta.get(key, None)
+
+    def keys(self) -> Generator[str, None, None]:
+
+        return (key for key in self._meta.keys())
